f8de55a702486eea318927a49d670494b45af257
Vasil Dimitrov <v.y.dimitrov@gmail.com>
2024-05-03 13:54:05 +0300

    Change ar model

--- src/app/routes/ar.tsx
+++ src/app/routes/ar.tsx
@@ -5,7 +5,7 @@ export function Ar() {
 
   return (
     <model-viewer
-      src="../glb/hack-robot.glb"
+      src="../glb/profession.glb"
       camera-orbit="65deg 0 0"
       shadow-intensity="3"
       shadow-softness="1.5"
