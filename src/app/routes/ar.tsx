7a5f12a7813c51432fe3474132171dc648082ac9
Vasil Dimitrov <v.y.dimitrov@gmail.com>
2024-05-03 16:50:43 +0300

    Fix qr-s

--- src/app/routes/ar.tsx
+++ src/app/routes/ar.tsx
@@ -6,12 +6,12 @@ export function Ar() {
         <img src="./glb/ar/qr-gold-chef.png" alt="QR gold chef" className="qr" />
       </div>
       <div>
-        <img src="./glb/ar/copper.webp" alt="QR copper robot" />
-        <img src="./glb/ar/qr-gold-chef.png" alt="QR gold chef" className="qr" />
+        <img src="./glb/ar/copper-robot.webp" alt="QR copper robot" />
+        <img src="./glb/ar/qr-copper-robot.png" alt="QR gold chef" className="qr" />
       </div>
       <div>
         <img src="./glb/ar/cyan-viking.webp" alt="QR cyan viking" />
-        <img src="./glb/ar/qr-gold-chef.png" alt="QR gold chef" className="qr" />
+        <img src="./glb/ar/qr-cyan-viking.png" alt="QR gold chef" className="qr" />
       </div>
     </div>
   );
