922114be8fca138fc91958921dbdc3fdd73efc2d
Vasil Dimitrov <v.y.dimitrov@gmail.com>
2024-05-03 13:18:49 +0300

    Change ar settings

--- src/app/routes/ar.tsx
+++ src/app/routes/ar.tsx
@@ -5,26 +5,18 @@ export function Ar() {
   const modelViewerRef = useModelViewer();
 
   return (
-    <>
-      <model-viewer
-        src="../glb/hack-robot.glb"
-        camera-orbit="65deg 0 0" // change the initial angle and position of the camera
-        shadow-intensity="3"
-        shadow-softness="1.5"
-        camera-controls
-        ref={modelViewerRef}
-        ar
-        touch-action="pan-y"
-      />
-      <Form
-        onSubmit={(value) => {
-          modelViewerRef.current?.model?.materials?.[0].pbrMetallicRoughness.setBaseColorFactor(value);
-        }}
-      >
-        <Input />
-        <Button>Change color</Button>
-      </Form>
-    </>
+    <model-viewer
+      src="../glb/hack-robot.glb"
+      camera-orbit="65deg 0 0" // change the initial angle and position of the camera
+      shadow-intensity="3"
+      shadow-softness="1.5"
+      camera-controls
+      ref={modelViewerRef}
+      ar
+      ar-scale="fixed"
+      touch-action="pan-y"
+      xr-environment
+    />
   );
 }
 
