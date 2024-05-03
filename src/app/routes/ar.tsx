a5cf84b7bba50dd2499dde857135da67c896d5de
Vasil Dimitrov <v.y.dimitrov@gmail.com>
2024-05-03 15:26:11 +0300

    Style ar page

--- src/app/routes/ar.tsx
+++ src/app/routes/ar.tsx
@@ -3,12 +3,15 @@ export function Ar() {
     <div className="ar">
       <div>
         <img src="./glb/ar/gold-chef.webp" alt="Gold chef" />
+        <img src="./glb/ar/qr-gold-chef.png" alt="QR gold chef" className="qr" />
       </div>
       <div>
-        <img src="./glb/ar/copper.webp" alt="Copper robot" />
+        <img src="./glb/ar/copper.webp" alt="QR copper robot" />
+        <img src="./glb/ar/qr-gold-chef.png" alt="QR gold chef" className="qr" />
       </div>
       <div>
-        <img src="./glb/ar/cyan-viking.webp" alt="Cyan viking" />
+        <img src="./glb/ar/cyan-viking.webp" alt="QR cyan viking" />
+        <img src="./glb/ar/qr-gold-chef.png" alt="QR gold chef" className="qr" />
       </div>
     </div>
   );
