3d76bcbd0993caa15dd5d0ef90e1026f5fc58c3c
Vasil Dimitrov <v.y.dimitrov@gmail.com>
2024-05-04 08:27:55 +0300

    Add example colors

--- src/app/routes/hack-robot.tsx
+++ src/app/routes/hack-robot.tsx
@@ -7,12 +7,12 @@ export function HackRobot() {
   return (
     <>
       <model-viewer
+        ref={modelViewerRef}
         src="../glb/hack-robot.glb"
         camera-orbit="65deg 0 0" // change the initial angle and position of the camera
         shadow-intensity="3"
         shadow-softness="1.5"
         camera-controls
-        ref={modelViewerRef}
       />
       <Form
         onSubmit={(value) => {
@@ -27,3 +27,5 @@ export function HackRobot() {
 }
 
 // $0.model.materials[0].pbrMetallicRoughness.setBaseColorFactor([Math.random(), Math.random(), Math.random()])
+
+// #FFC0CB  #F4C0FF
