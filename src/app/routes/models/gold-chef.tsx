3e8474710cfa96c25d10b310afa04ee8750d1845
Vasil Dimitrov <v.y.dimitrov@gmail.com>
2024-05-06 18:13:05 +0300

    Add ar.glb

--- src/app/routes/models/gold-chef.tsx
+++ src/app/routes/models/gold-chef.tsx
@@ -1,5 +1,3 @@
 export function GoldChef() {
-  return (
-    <model-viewer ar src="../glb/hack-robot.glb" camera-orbit="65deg 0 0" shadow-intensity="3" shadow-softness="1.5" camera-controls />
-  );
+  return <model-viewer ar src="../glb/ar/ar.glb" camera-orbit="65deg 0 0" shadow-intensity="3" shadow-softness="1.5" camera-controls />;
 }
