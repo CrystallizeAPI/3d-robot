30e77ee4eae4ee80db6d7ea93c1f82120e074224
Vasil Dimitrov <v.y.dimitrov@gmail.com>
2024-05-06 18:17:59 +0300

    Center ar

--- src/app/routes/models/gold-chef.tsx
+++ src/app/routes/models/gold-chef.tsx
@@ -1,3 +1,7 @@
 export function GoldChef() {
-  return <model-viewer ar src="../glb/ar/ar.glb" camera-orbit="65deg 0 0" shadow-intensity="3" shadow-softness="1.5" camera-controls />;
+  return (
+    <div className="ar">
+      <model-viewer ar src="../glb/ar/ar.glb" camera-orbit="65deg 0 0" shadow-intensity="3" shadow-softness="1.5" camera-controls />;
+    </div>
+  );
 }
