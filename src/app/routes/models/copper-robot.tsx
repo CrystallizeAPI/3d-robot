ddb05e9562290719699d468f4a7e4cd02c2a6084
Vasil Dimitrov <v.y.dimitrov@gmail.com>
2024-05-07 17:27:55 +0300

    Add gold chef for AR

--- src/app/routes/models/copper-robot.tsx
+++ src/app/routes/models/copper-robot.tsx
@@ -1,5 +1,7 @@
 export function CopperRobot() {
   return (
-    <model-viewer ar src="../glb/hack-robot.glb" camera-orbit="65deg 0 0" shadow-intensity="3" shadow-softness="1.5" camera-controls />
+    <div className="ar">
+      <model-viewer ar src="../glb/hack-robot.glb" camera-orbit="65deg 0 0" shadow-intensity="3" shadow-softness="1.5" camera-controls />
+    </div>
   );
 }
--- src/app/routes/models/gold-chef.tsx
+++ src/app/routes/models/gold-chef.tsx
@@ -1,7 +1,7 @@
 export function GoldChef() {
   return (
     <div className="ar">
-      <model-viewer ar src="../glb/ar/ar.glb" camera-orbit="65deg 0 0" shadow-intensity="3" shadow-softness="1.5" camera-controls />;
+      <model-viewer ar src="../glb/ar/gold-chef.glb" camera-orbit="65deg 0 0" shadow-intensity="3" shadow-softness="1.5" camera-controls />;
     </div>
   );
 }
