79191a3599653508a764ed2367f6fa005dee2d79
Vasil Dimitrov <v.y.dimitrov@gmail.com>
2024-05-07 17:46:00 +0300

    Add cyan viking

--- src/app/routes/models/cyan-viking.tsx
+++ src/app/routes/models/cyan-viking.tsx
@@ -1,7 +1,14 @@
 export function CyanViking() {
   return (
     <div className="ar">
-      <model-viewer ar src="../glb/hack-robot.glb" camera-orbit="65deg 0 0" shadow-intensity="3" shadow-softness="1.5" camera-controls />
+      <model-viewer
+        ar
+        src="../glb/ar/cyan-viking.glb"
+        camera-orbit="65deg 0 0"
+        shadow-intensity="3"
+        shadow-softness="1.5"
+        camera-controls
+      />
     </div>
   );
 }
