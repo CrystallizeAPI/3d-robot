33e190cb45bfb4cfd15cc07bab7be6b9fc21b063
Vasil Dimitrov <v.y.dimitrov@gmail.com>
2024-05-07 17:41:38 +0300

    Add copper robot

--- src/app/routes/models/copper-robot.tsx
+++ src/app/routes/models/copper-robot.tsx
@@ -1,7 +1,14 @@
 export function CopperRobot() {
   return (
     <div className="ar">
-      <model-viewer ar src="../glb/hack-robot.glb" camera-orbit="65deg 0 0" shadow-intensity="3" shadow-softness="1.5" camera-controls />
+      <model-viewer
+        ar
+        src="../glb/ar/copper-robot.glb"
+        camera-orbit="65deg 0 0"
+        shadow-intensity="3"
+        shadow-softness="1.5"
+        camera-controls
+      />
     </div>
   );
 }
