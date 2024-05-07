2c92a642984620161cdec0fed422266d97058aeb
Vasil Dimitrov <v.y.dimitrov@gmail.com>
2024-05-07 17:54:06 +0300

    Add names to robots

--- src/app/routes/ar.tsx
+++ src/app/routes/ar.tsx
@@ -2,14 +2,17 @@ export function Ar() {
   return (
     <div className="ar">
       <div>
+        <h2>Master chef</h2>
         <img src="./glb/ar/gold-chef.webp" alt="Gold chef" />
         <img src="./glb/ar/qr-gold-chef.png" alt="QR gold chef" className="qr" />
       </div>
       <div>
+        <h2>Jack of all trades</h2>
         <img src="./glb/ar/copper-robot.webp" alt="QR copper robot" />
         <img src="./glb/ar/qr-copper-robot.png" alt="QR gold chef" className="qr" />
       </div>
       <div>
+        <h2>Cave guardian</h2>
         <img src="./glb/ar/cyan-viking.webp" alt="QR cyan viking" />
         <img src="./glb/ar/qr-cyan-viking.png" alt="QR gold chef" className="qr" />
       </div>
