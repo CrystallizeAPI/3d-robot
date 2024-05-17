3f325b0ff90d314883cc9743ab0e696817db2f77
Vasil Dimitrov <v.y.dimitrov@gmail.com>
2024-05-17 10:14:42 +0300

    Clean glb files

--- src/app/routes/robot.tsx
+++ src/app/routes/robot.tsx
@@ -1,15 +1,15 @@
 export function Robot() {
-  return (
-    <>
-      <model-viewer
-        src="./glb/robot.glb"
-        camera-orbit="65deg 0 0"
-        shadow-intensity="3"
-        shadow-softness="1.5"
-        camera-controls
-      />
-    </>
-  );
+    return (
+        <>
+            <model-viewer
+                src="./robot.glb"
+                camera-orbit="65deg 0 0"
+                shadow-intensity="3"
+                shadow-softness="1.5"
+                camera-controls
+            />
+        </>
+    );
 }
 
 //
