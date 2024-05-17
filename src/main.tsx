aa04ad945692f168b300f79a6dd69821cbd90001
Vasil Dimitrov <v.y.dimitrov@gmail.com>
2024-05-17 12:07:25 +0300

    Revert to package usage

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+import "@google/model-viewer/lib/model-viewer";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
